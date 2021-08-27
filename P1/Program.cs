a72687027e9609872c54302c04d87f3744f0067c
ferizoozoo <farhadzohoorz@gmail.com>
2021-08-27 07:31:32 +0430

    Add solution to problem 1

---
+++ P1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace P1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 1000;
+            int sumOfMultiplesOfThreeOrFiveBelowN = 0;
+
+            for (int number = 1; number < N; number++)
+            {
+                if (number % 3 == 0 || number % 5 == 0)
+                    sumOfMultiplesOfThreeOrFiveBelowN += number;
+            }
+
+            Console.WriteLine(sumOfMultiplesOfThreeOrFiveBelowN);
+        }
+    }
+}
