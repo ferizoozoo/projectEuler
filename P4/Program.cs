b1abb0dd495eaf43275328b50baffbebb999b360
ferizoozoo <farhadzohoorz@gmail.com>
2021-08-29 22:59:19 +0430

    Add solution to problem 4

---
+++ P4/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
+
+namespace P4
+{
+    class Program
+    {
+        static string GetReversedString(string s)
+        {
+            var charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        static bool IsPalindrome(int number)
+        {
+            var numberString = number.ToString();
+            return numberString == GetReversedString(numberString);
+        }
+
+        static IEnumerable<int> GenerateProductOfThreeDigitNumbers()
+        {
+            for (int firstNumber = 999; firstNumber >= 100; firstNumber--)
+            {
+                for (int secondNumber = 999; secondNumber >= 100; secondNumber--)
+                    yield return firstNumber * secondNumber;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var listOfPalindromeNumbers = new List<int>();
+
+            foreach (var number in GenerateProductOfThreeDigitNumbers())
+            {
+                if (IsPalindrome(number))
+                {
+                    listOfPalindromeNumbers.Add(number);
+                }
+            }
+
+            Console.WriteLine(listOfPalindromeNumbers.Max());
+        }
+    }
+}
