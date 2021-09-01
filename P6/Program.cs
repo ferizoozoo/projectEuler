5178103d4e0811841e6b6cdb36b95cbbc426bf23
ferizoozoo <farhadzohoorz@gmail.com>
2021-09-01 14:13:24 +0430

    Add solution to problem 6

---
+++ P6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace P6
+{
+    class Program
+    {
+        static long SumOfSquaresOfFirst_N_Numbers(int n)
+        {
+            long result = 0;
+
+            for (int number = 1; number <= n; number++)
+                result += (long)Math.Pow(number, 2);
+
+            return result;
+        }
+
+        static long SquareOfSumOfFirst_N_Numbers(int n)
+        {
+            var sumOfFirst_N_Numbers = 0;
+
+            for (int number = 1; number <= n; number++)
+                sumOfFirst_N_Numbers += number;
+
+            return (long)Math.Pow(sumOfFirst_N_Numbers, 2);
+        }
+
+        static void Main(string[] args)
+        {
+            int n = 100;
+            Console.WriteLine(SquareOfSumOfFirst_N_Numbers(n) - SumOfSquaresOfFirst_N_Numbers(n));
+        }
+    }
+}
